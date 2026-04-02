abadf86f01e5bdc1266289eaf3074f101818dfe5
Kishan Pun <rockzmgr@gmail.com>
2026-04-02 18:14:59 +0530

    fix: dashboard redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 "use client";
-// app/page.tsx
-import { supabase } from "@/lib/supabase";
+
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export default async function Home() {
+export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
     router.push("/dashboard");
   }, []);
 
-  const { data, error } = await supabase.from("test").select("*");
-
-  console.log("DATA:", data);
-  console.log("ERROR:", error);
-
-  return <p className="text-center mt-10">Loading...</p>;
+  return (
+    <p className="text-center mt-10">
+      Redirecting to dashboard...
+    </p>
+  );
 }
